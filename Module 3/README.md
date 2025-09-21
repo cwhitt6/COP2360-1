4f379870400731809255de2ae7a9bcc9538ad256
cwhitt6 <bitzs6380@gmail.com>
2025-09-21 14:53:58 -0400

    Add Module 3 folder with README and Test File

---
+++ Module 3/README.md
@@ -0,0 +1,3 @@
+# Module 3
+
+This folder contains resources for Module 3.
---
+++ Module 3/Test File.cs
@@ -0,0 +1,15 @@
+// Test File.cs
+// Placeholder C# file for Module 3
+
+using System;
+
+namespace Module3
+{
+    class TestFile
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Test File.cs in Module 3!");
+        }
+    }
+}
